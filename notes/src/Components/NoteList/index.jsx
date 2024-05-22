7e8daf8cb2090e6d83091e389e242668c49de0a9
Luis Ricarte <luis.ricarte@ccc.ufcg.edu.br>
2024-05-21 22:44:32 -0300

    adicionando estilo

--- notes/src/Components/NoteList/index.jsx
+++ notes/src/Components/NoteList/index.jsx
@@ -8,16 +8,8 @@ const ListNote = () => {
             <h2 style={{fontFamily: 'Roboto', marginBottom: '0'}}>Título</h2>
             <input type="text" placeholder='Título' className='titleInput'/>
             <h2 style={{fontFamily: 'Roboto', marginBottom: '0'}}>Descrição</h2>
-            <textarea rows={5} cols={32} placeholder='adicione sua descrição'
-                            style={{
-                                backgroundColor:'#bdbdbd', 
-                                borderRadius: '1rem', 
-                                border: 0, 
-                                paddingLeft:'1rem', 
-                                paddingTop:'0.5rem', 
-                                paddingBottom:'0.5rem',
-                                }}></textarea>
-            <button style={{}} >Adicionar</button>
+            <textarea rows={5} cols={32} placeholder='adicione sua descrição' className='descriptionInput'></textarea>
+            <button className='button'>Adicionar</button>
         </form>
         </>
     )
